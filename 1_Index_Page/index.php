1948d8d1c1464cd5084e586571af622bbc48e533
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-06-27 10:33:09 +0545

    Index page with echo, comments and including a Html File within a PHP file.

---
+++ 1_Index_Page/index.php
@@ -0,0 +1,27 @@
+<?php
+    echo "I Love Coding <br>";
+    echo "Now I am Learning PHP Programming Language";
+
+    // This is a single line comment in PHP
+    /*
+        This 
+        is 
+        a 
+        multiline
+        comment in
+        PHP 
+    */
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Practicing PHP</title>
+</head>
+<body>
+    <br><br>
+    <button>I am a Button</button>
+</body>
+</html>
