2c208e99bf4fd5402fe66ee84e98b68470779099
AnishKhatri17 <anish.khatri.135@gmail.com>
2025-07-04 22:23:00 +0545

    The use of Arithmetic Operators in PHP

---
+++ Arithmetic.php
@@ -0,0 +1,17 @@
+<?php
+echo "Multiplying Two different numbers. <br><br>";
+$a = 10 ;
+$b = 20 ;
+$c = $a+$b ;
+$d = $a-$b ;
+$e = $a*$b ;
+$f = $a/$b ;
+echo("The addition of two numbers is :".$c);
+echo("<br>");
+echo("The subtraction of two numbers is:".$d);
+echo("<br>");
+echo("The multiplication of two numbers is:".$e);
+echo("<br>");
+echo("The division of two numbers is :".$f);
+
+?>
